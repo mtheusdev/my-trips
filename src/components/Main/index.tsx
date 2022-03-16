e03779045e0a5ff5feac4a8db8c13c9217d5f89b
mtheusdev <matheus.santos@edu.udesc.br>
2022-03-16 16:28:54 -0300

    Initial commit from Create Next App

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,7 @@
+const Main = () => (
+  <main>
+    <h1>mytrips</h1>
+  </main>
+)
+
+export default Main
---
+++ src/pages/_app.tsx
@@ -0,0 +1,19 @@
+import type { AppProps } from 'next/app'
+import Head from 'next/head'
+
+import GlobalStyles from 'styles/global'
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>BoilerPlate</title>
+        <link rel="shortcut icon" href="/img/icon-512.png"></link>
+        <link rel="apple-touch-icon" href="/img/icon-512.png"></link>
+        <meta name="description" content="A simple project" />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  )
+}

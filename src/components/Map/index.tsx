d825097080d0927ee546c0386d6d8f618b6501c5
mtheusdev <matheus.santos@edu.udesc.br>
2022-03-17 16:51:03 -0300

    Adicionando evento de click no marquer e redirecionando para página correta

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,4 +1,5 @@
 import { MapContainer, TileLayer, Marker } from 'react-leaflet'
+import { useRouter } from 'next/dist/client/router'
 
 type Place = {
   id: string
@@ -14,27 +15,36 @@ export type MapProps = {
   places?: Place[]
 }
 
-const Map = ({ places }: MapProps) => (
-  <MapContainer
-    center={[0, 0]}
-    zoom={3}
-    style={{ height: '100%', width: '100%' }}
-  >
-    <TileLayer
-      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    />
-    {places?.map(({ id, name, location }) => {
-      const { latitude, longitude } = location
-      return (
-        <Marker
-          key={`place-${id}`}
-          position={[latitude, longitude]}
-          title={name}
-        />
-      )
-    })}
-  </MapContainer>
-)
+const Map = ({ places }: MapProps) => {
+  const router = useRouter()
+
+  return (
+    <MapContainer
+      center={[0, 0]}
+      zoom={3}
+      style={{ height: '100%', width: '100%' }}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {places?.map(({ id, name, slug, location }) => {
+        const { latitude, longitude } = location
+        return (
+          <Marker
+            key={`place-${id}`}
+            position={[latitude, longitude]}
+            title={name}
+            eventHandlers={{
+              click: () => {
+                router.push(`/place/${slug}`)
+              }
+            }}
+          />
+        )
+      })}
+    </MapContainer>
+  )
+}
 
 export default Map
